57ce4a75fd26739d67eda175131b7cee37930552
yujirou <yujirou@example.com>
2023-11-12 19:06:52 +0900

    first commit

---
+++ src/main/java/com/example/ShortStayApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortStayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShortStayApplication.class, args);
+	}
+
+}
